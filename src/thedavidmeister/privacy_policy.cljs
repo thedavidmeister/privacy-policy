e7bd9a472696cfdc4f6e708a3ed8e47f80705707
David Meister <thedavidmeister@gmail.com>
2017-09-18 19:58:08 +1000

    added a few lines

--- src/thedavidmeister/privacy_policy.cljs
+++ src/thedavidmeister/privacy_policy.cljs
@@ -11,4 +11,8 @@
 
   (h/h2 "What is our commitment to the protection of your personal information?")
   (h/p (entity-name) " is " (entity-description))
-  (h/p "By accessing our web application, engaging our services, or providing any personal information to us, you agree to be bound by this privacy policy and consent to our use and disclosure of your personal information as explained within this policy.")])
+  (h/p "By accessing our web application, engaging our services, or providing any personal information to us, you agree to be bound by this privacy policy and consent to our use and disclosure of your personal information as explained within this policy.")
+
+  (h/h2 "We are committed to protecting your privacy.")
+  (h/p "This Privacy Policy explains how we collect, use, and disclose personal information. It also explains your rights and our processes to request access, updates and corrections to your details.")
+  (h/p "It is important to us that you are confident that any personal and sensitive information we hold about you will be treated in a way which ensures protection of your personal information.")])
